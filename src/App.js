c588ae8e6fd422e4dfcf650e7ec70d41d34eab62
Nicolas Burghi <nicoburghi@gmail.com>
2018-11-22 20:23:03 -0300

    update general app configs

--- src/App.js
+++ src/App.js
@@ -1,85 +1,109 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Loader, Segment } from 'semantic-ui-react';
 import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
-
-import BeerGrid from './components/BeerGrid'
+import BeerGrid from './components/BeerGrid';
+import Filter from './components/Filter';
 
 const API_URL = 'https://api.punkapi.com/v2/beers';
 
-class App extends Component {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-  constructor(props){
-    super(props);
+        this.state = {
+            isFetching: false,
+            data: null,
+            hasError: false,
+        };
 
-    this.state = {
-      isFetching: false,
-      data: null,
-      hasError: false
+        this.onFilter = this.onFilter.bind(this);
+        this.getBeers = this.getBeers.bind(this);
     }
-  }
 
-  componentDidMount(){
-    this.getBeers({});
-  }
+    componentDidMount() {
+        this.getBeers({});
+    }
 
-  getBeers(filters){
-    
-    this.setState({
-      isFetching: true
-    });
+    onFilter(filters) {
+        this.getBeers(filters);
+    }
 
-    fetch(API_URL).then((response) => {
-      return response.json();
-    }).then((data) => {
-      console.log('parsed json', data);
+    getBeers(filters = {}) {
+        const getQueryParams = () => {
+            const queryParams = Object.keys(filters)
+                .filter(key => filters[key] !== '')
+                .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(filters[key])}`)
+                .join('&');
+            return queryParams ? `?${queryParams}` : '';
+        };
 
-      this.setState({
-        data,
-        isFetching: false,
-        hasError: false
-      });
+        const requestUrl = `${API_URL}${getQueryParams()}`;
 
-    }).catch((ex) => {
-      console.log('parsing failed', ex);
+        this.setState({
+            isFetching: true,
+            hasError: false,
+        });
 
-      this.setState({
-        isFetching: false,
-        hasError: true
-      });
+        window.fetch(requestUrl)
+            .then(response => response.json())
+            .then((data) => {
+                if (data.error) {
+                    this.setState({
+                        isFetching: false,
+                        hasError: true,
+                    });
+                } else {
+                    this.setState({
+                        data,
+                        isFetching: false,
+                    });
+                }
+            }).catch(() => {
+                this.setState({
+                    isFetching: false,
+                    hasError: true,
+                });
+            });
+    }
 
-    });
+    render() {
+        const { data, isFetching, hasError } = this.state;
 
-  }
-
-  render() {
-
-    const { data, isFetching, hasError } = this.state;
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {
-          isFetching
-          && <span>Loading</span>
-        }
-        {
-          !isFetching && data
-          && <BeerGrid beers={data} />
-        }
-        {
-          hasError
-          && <span>Error</span>
-        }
-        
-      </div>
-    );
-  }
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit
+                    {' '}
+                    <code>src/App.js</code>
+                    {' '}
+                    and save to reload.
+                </p>
+                <Filter onFilter={this.onFilter} />
+                {
+                    isFetching
+                    && <Loader active inline='centered' />
+                }
+                {
+                    !isFetching && data
+                    && <BeerGrid beers={data} />
+                }
+                {
+                    hasError
+                    && (
+                        <Segment inverted color='red'>
+                            Oops there was a problem serving the beer! Please try again in a few minutes.
+                        </Segment>
+                    )
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/BeerGrid.js
+++ src/components/BeerGrid.js
@@ -1,25 +1,80 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Container, Grid, Card, Image } from 'semantic-ui-react';
+import BeerDetails from './BeerDetails';
 
-class BeerGrid extends Component {
-
-    constructor(props){
+class BeerGrid extends React.Component {
+    constructor(props) {
         super(props);
 
         this.state = {
-            beers: null
-        }
+            isModalOpened: false,
+        };
+
+        this.beerDetailData = null;
+        this.onCloseDetail = this.onCloseDetail.bind(this);
     }
 
-    render(){
+    onCloseDetail() {
+        this.beerDetailData = null;
+        this.setState({
+            isModalOpened: false,
+        });
+    }
 
+    handleClick(event, beer) {
+        event.preventDefault();
+
+        this.beerDetailData = beer;
+        this.setState({
+            isModalOpened: true,
+        });
+    }
+
+    render() {
         const { beers } = this.props;
+        const { isModalOpened } = this.state;
 
         return (
-            <div className="beer-grid">
-                <pre>{ JSON.stringify(beers) }</pre>
-            </div>
+            <Container>
+                <Grid stackable columns={4}>
+                    {
+                        beers.map(beer => (
+                            <Grid.Column key={beer.id}>
+                                <Card link onClick={e => this.handleClick(e, beer)}>
+                                    <div>
+                                        <Image centered style={{ maxHeight: 200 }} src={beer.image_url} />
+                                    </div>
+                                    <Card.Content>
+                                        <Card.Header>{beer.name}</Card.Header>
+                                        <Card.Meta>
+                                            <span className='date'>{`Since ${beer.first_brewed}`}</span>
+                                        </Card.Meta>
+                                        <Card.Description>{beer.tagline}</Card.Description>
+                                    </Card.Content>
+                                    <Card.Content extra>
+                                        { `${beer.abv} % - ${beer.ibu} IBU` }
+                                    </Card.Content>
+                                </Card>
+                            </Grid.Column>
+                        ))
+                    }
+                </Grid>
+                {
+                    isModalOpened
+                    && <BeerDetails beer={this.beerDetailData} onCloseDetail={this.onCloseDetail} />
+                }
+            </Container>
         );
     }
 }
+
+BeerGrid.defaultProps = {
+    beers: [],
+};
+
+BeerGrid.propTypes = {
+    beers: PropTypes.arrayOf(PropTypes.shape({})),
+};
 
 export default BeerGrid;
