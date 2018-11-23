d91a4c195795543b28beb2c5d8b704c83394d132
Nicolas Burghi <nicoburghi@gmail.com>
2018-11-22 21:50:48 -0300

    add new filters

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,31 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Form, Input, Button } from 'semantic-ui-react';
 
 class Filter extends React.Component {
     constructor(props) {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.beerNameField = React.createRef();
+        this.ibuGtField = React.createRef();
+        this.abvGtField = React.createRef();
     }
 
     handleSubmit(event) {
         event.preventDefault();
         const { onFilter } = this.props;
         const filters = {
-            beer_name: this.beerNameField.current.value,
+            beer_name: this.beerNameField.current.inputRef.value,
+            ibu_gt: this.ibuGtField.current.inputRef.value,
+            abv_gt: this.abvGtField.current.inputRef.value,
         };
         onFilter(filters);
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label htmlFor="beer_name">
-                    Name:
-                    <input type="text" id="beer_name" ref={this.beerNameField} />
-                </label>
-                <input type="submit" value="Filter" />
-            </form>
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group widths='equal'>
+                    <Form.Field>
+                        <Input placeholder='Name' ref={this.beerNameField} />
+                    </Form.Field>
+                    <Form.Field>
+                        <Input placeholder='IBU Greater Than' ref={this.ibuGtField} />
+                    </Form.Field>
+                    <Form.Field>
+                        <Input placeholder='ABV Greater Than' ref={this.abvGtField} />
+                    </Form.Field>
+                    <Form.Field>
+                        <Button>Filter</Button>
+                    </Form.Field>
+                </Form.Group>
+            </Form>
         );
     }
 }
